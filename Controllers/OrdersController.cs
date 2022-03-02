11bd13ae3cd0ed191cfd4826c4ffa1931953126f
Duong <babyduong29@gmail.com>
2022-03-02 12:05:43 +0700

    fixsearrch fontend

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,55 @@
+﻿using FptBookNew1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FptBookNew1.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private ModelDatabase _db = new ModelDatabase();
+        // GET: Orders
+        public ActionResult Index()
+        {
+            if (Session["UserNameAdmin"] != null)
+            {
+                var orders = _db.orders.Include(o => o.account);
+                return View(orders.ToList());
+            }
+            return View("Error");
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (Session["Admin"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                order order = _db.orders.Find(id);
+                if (order == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(order);
+            }
+            return View("Error");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -67,6 +68,83 @@ namespace FptBookNew1.Controllers
             ViewBag.TotalItem = total_item;
 
             return PartialView("BagCart");
+        }
+        public ActionResult Checkout(FormCollection form)
+        {
+            try
+            {
+                Cart cart = Session["Cart"] as Cart;
+                order _order = new order();
+                _order.orderDate = DateTime.Now;
+                _order.username = form["Username"];
+                _order.address = form["Address"];
+                _order.phone = form["Phone"];
+                _order.totalPrice = Convert.ToInt32(form["TotalPrice"]);
+                db.orders.Add(_order);
+
+                foreach (var item in cart.Items)
+                {
+                    orderDetail orderDetail = new orderDetail();
+                    orderDetail.orderID = _order.orderID;
+                    orderDetail.bookID = item._shopping_product.bookID;
+                    orderDetail.quantity = item._shopping_quantity;
+                    orderDetail.amountPrice = item._shopping_product.price * item._shopping_quantity;
+
+                    var pro = db.books.SingleOrDefault(s => s.bookID == orderDetail.bookID);
+
+                    pro.quantity -= orderDetail.quantity;
+                    db.books.Attach(pro);
+                    db.Entry(pro).Property(a => a.quantity).IsModified = true;
+
+                    db.orderDetails.Add(orderDetail);
+
+
+                }
+
+                db.SaveChanges();
+                cart.ClearCart();
+                return RedirectToAction("CheckoutSuccess", "ShoppingCart", new { id = _order.orderID });
+            }
+            catch
+            {
+                return Content("Error checkout, Check information again");
+            }
+        }
+        public ActionResult CheckoutSuccess(int? id)
+        {
+            if (Session["Username"] != null)
+            {
+                var order = db.orders.Find(id);
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                if (order == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(order);
+            }
+            return View("ErrorCart");
+        }
+
+        public ActionResult OrderHistory(string id)
+        {
+            if (Session["Username"] != null)
+            {
+                var orderHis = db.orders.ToList().Where(s => s.username == id);
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                if (orderHis == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(orderHis);
+            }
+            return View("ErrorCart");
         }
     }
 }
